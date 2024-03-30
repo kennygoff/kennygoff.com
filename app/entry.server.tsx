ea3cb551fa456340434d5027faecd35d2c194334
Kenny Goff <me@kennygoff.com>
2024-03-29 21:37:59 -0400

    Fix netlify for Remix Vite

--- app/entry.server.tsx
+++
@@ -1,2 +0,0 @@
-// This is necessary as netlify's custom adapter fixes a bug
-export { handleRequest as default } from "@netlify/remix-adapter";
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import { config } from "@netlify/remix-adapter";
 import tsconfigPaths from "vite-tsconfig-paths";
 import mdx from "@mdx-js/rollup";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import inspect from "vite-plugin-inspect";
 import { visualizer } from "rollup-plugin-visualizer";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig((env) => ({
   server: {
@@ -27,10 +27,8 @@ export default defineConfig((env) => ({
     //     ],
     //   }),
     // },
-    remix({
-      ...(env.command === "build" ? config : undefined),
-      // ...(process.env.NODE_ENV === "production" ? config : undefined),
-    }),
+    remix(),
+    netlifyPlugin(),
     tsconfigPaths(),
     visualizer({ emitFile: true }),
   ],
