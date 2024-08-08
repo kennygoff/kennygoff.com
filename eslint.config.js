3f6ad0986a59894b62fceeba2db0c914a19c3273
Kenny Goff <me@kennygoff.com>
2024-08-07 21:20:12 -0400

    [#22] Migrate to eslint v9
    Closes #22

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config} */
+export default [
+  eslintConfigPrettier,
+  {
+    ignores: ["build/**/*", "public/**/*"],
+  },
+];
