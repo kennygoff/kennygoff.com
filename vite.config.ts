f815c8ca75cac5e1d7f04662aef719d46c23cbc8
Kenny Goff <me@kennygoff.com>
2024-11-05 20:41:02 -0500

    Upgrade dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ import inspect from "vite-plugin-inspect";
 import { visualizer } from "rollup-plugin-visualizer";
 import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
+declare module "@remix-run/server-runtime" {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
 export default defineConfig(() => ({
   server: {
     port: 3000,
@@ -27,7 +34,15 @@ export default defineConfig(() => ({
     //     ],
     //   }),
     // },
-    remix(),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_lazyRouteDiscovery: true,
+        v3_relativeSplatPath: true,
+        v3_singleFetch: true,
+        v3_throwAbortReason: true,
+      },
+    }),
     netlifyPlugin(),
     tsconfigPaths(),
     visualizer({ emitFile: true }),
