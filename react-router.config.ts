debf9d77dc91dd37e208169ee0fd6cc689f31726
Kenny Goff <me@kennygoff.com>
2024-11-23 18:35:41 -0500

    Disable prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
+  // Does not work, seems to not work with either netlify config or custom server
   // async prerender() {
   //   return ["/"];
   // },
