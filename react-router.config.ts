8f95e63b7a197e5e0234e08db5c2257a7efa5401
Kenny Goff <me@kennygoff.com>
2024-11-23 18:27:37 -0500

    Fix netlify SSR build

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  ssr: true,
-  async prerender() {
-    return ["/"];
-  },
+  // async prerender() {
+  //   return ["/"];
+  // },
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import mdx from "@mdx-js/rollup";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import inspect from "vite-plugin-inspect";
 import { visualizer } from "rollup-plugin-visualizer";
+import { reactRouter } from "@react-router/dev/vite";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(() => ({
-  server: {
-    port: 3000,
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
   },
   plugins: [
     inspect(),
