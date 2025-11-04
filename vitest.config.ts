64b11837c95a0845594e3bfe2b7ea91e81497cc9
Kenny Goff <me@kennygoff.com>
2025-11-04 09:40:30 -0500

    Update vitest to v4

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig(() => ({
   test: {
     environment: "happy-dom",
+    coverage: {
+      include: ["app/**/*.{js,jsx,ts,tsx}"],
+    },
   },
 }));
